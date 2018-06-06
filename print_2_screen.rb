17b78fc07124d7897008f02951a1e4c37920eb33
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-06 12:43:52 -0400

    Add files via upload
    First file for June 6 in class, learning put, print and p commands, plus if and if-else loops.

---
+++ print_2_screen.rb
@@ -0,0 +1,25 @@
+p 1
+puts 1
+print 1
+
+100.times do
+ puts 1
+end
+
+number = 1
+100.times do
+  puts number
+  number = number + 1
+end
+
+#replace 3 with "mind"
+
+number = 1
+100.times do
+  if number == 3
+    puts "mined"
+  else
+    puts number
+  end
+    number = number + 1
+end
